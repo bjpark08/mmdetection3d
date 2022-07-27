35517a93f3a14bd035a08a7d7db266d5989f759a
Beomjun Park <bjpark0805@rideflux.com>
2022-07-27 09:58:43 +0000

    [pointpillar] pointpillar pixel size 0.2로 줄여서 실험

--- configs/pointpillars/hv_pointpillars_secfpn_sbn_6x2_2x_rf2021-3d-2class.py
+++ configs/pointpillars/hv_pointpillars_secfpn_sbn_6x2_2x_rf2021-3d-2class.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/hv_pointpillars_secfpn_rf2021_mvxfasterrcnn.py',    
+    '../_base_/models/hv_pointpillars_02_secfpn_rf2021_mvxfasterrcnn.py',    
     '../_base_/datasets/rf2021-3d-2class.py',
     '../_base_/schedules/schedule_2x.py',
     '../_base_/default_runtime.py',
