9bc7c1cd96d3cf35b7b3a22ef083ad419984ed22
Beomjun Park <bjpark0805@rideflux.com>
2022-10-27 17:08:50 +0900

    [Relabeling] utility용 함수 제작 (보행자 extractor)

--- tools/dataset_modifier_analyzer/extract_noncar_extractor.py
+++ tools/dataset_modifier_analyzer/extract_noncar_extractor.py
@@ -1,11 +1,12 @@
 import pickle
 import numpy
 
-def extract_ped(datas,ped_cnt,data_cnt):
+def extract_noncar(datas,cyc_cnt,ped_cnt,data_cnt):
 	result=[]
 	for data in datas:
+		cyc_idx=numpy.where(data['annos']['gt_names']=='Cyclist')
 		ped_idx=numpy.where(data['annos']['gt_names']=='Pedestrian')
-		if (len(ped_idx[0])>=ped_cnt):
+		if (len(cyc_idx[0])>=cyc_cnt) and (len(ped_idx[0])>=ped_cnt):
 			cur_data=data
 			#cur_data['annos']['gt_bboxes_3d']=numpy.delete(cur_data['annos']['gt_bboxes_3d'],range(ped_idx[0][0]),axis=0)
 			#cur_data['annos']['gt_names']=numpy.delete(cur_data['annos']['gt_names'],range(ped_idx[0][0]),axis=0)
@@ -14,16 +15,17 @@ def extract_ped(datas,ped_cnt,data_cnt):
 				break
 	return result
 
-file_name='data/rf2021/rf2021_infos_test_height_make'
+file_name='data/rf2021/rf2021_relabeled_infos_train'
 
 with open(file_name+'.pkl','rb') as f:
 	datas=pickle.load(f)
 
+cyc_cnt=0
 ped_cnt=10
-data_cnt=100
-ped_data=extract_ped(datas,ped_cnt,data_cnt)
-print(ped_cnt, data_cnt, len(ped_data))
+data_cnt=10000
+ped_data=extract_noncar(datas,cyc_cnt,ped_cnt,data_cnt)
+print(cyc_cnt, ped_cnt, data_cnt, len(ped_data))
 
 
-with open(file_name+'_ped_'+str(ped_cnt)+'.pkl','wb') as rf:
+with open(file_name+'_noncar_'+str(ped_cnt)+'.pkl','wb') as rf:
 	pickle.dump(ped_data,rf,protocol=pickle.HIGHEST_PROTOCOL)
